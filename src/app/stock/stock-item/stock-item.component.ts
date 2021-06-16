3199e5657a4b18ddb232d08fdf6e5b324f9927ef
kw00183 <kw00183@cs6261-vm08.uwg.westga.edu>
2021-06-15 22:44:33 -0400

    change the stock-item code and the app code to all the stock-item component to accept input and output so we can pass data into the component and listen for changes from the component

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Stock } from '../../model/stock';
 
@@ -7,40 +7,16 @@ import { Stock } from '../../model/stock';
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css']
 })
-export class StockItemComponent implements OnInit {
+export class StockItemComponent {
 
-  public stocks: Array<Stock>;
-  public stockClasses;
-  public stockStyles;
+  @Input() public stock: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  constructor() { }
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
+   }
 
-  ngOnInit() {
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80),
-      new Stock('Second Stock Company', 'SSC', 10, 20),
-      new Stock('Last Stock Company', 'LSC', 876, 765)
-    ];
-    let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
-    };
-    this.stockStyles = {
-      "color": this.stock.isPositiveChange() ? "green" : "red",
-      "font-size": largeChange ? "1.2em" : "0.8em"
-    };
-  }
-
-  toggleFavorite(event, index) {
-    console.log('We are toggling the favorite state for this stock', index , event);
-    this.stock[index].favorite = !this.stock[index].favorite;
-  }
-
-  trackStockByCode(index, stock) {
-    return stock.code;
+  onToggleFavorite(event) {
+    this.toggleFavorite.emit(this.stock);
   }
 }
