c01ec9806c89e691d908352e70d38a1bffb345b7
kw00183 <kw00183@cs6261-vm08.uwg.westga.edu>
2021-06-16 19:28:47 -0400

    add in the create stock component and form elements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,54 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { StockItemComponent } from '../app/stock/stock-item/stock-item.component';
 import { Stock } from '../app/model/stock';
+import { By } from '@angular/platform-browser';
+
+describe('AppComponent', () => {
+
+  describe('Simple, No Angular Unit Test', () => {
+    /** Move all the previous test code into a
+        child describe block
+    */
+  });
+
+  describe('Angular-Aware test', () => {
+
+    let fixture, component;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          StockItemComponent,
+        ],
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should load stock with default values', () => {
+      const titleEl = fixture.debugElement.query(By.css('h1'));
+      // Trim to avoid HTML whitespaces
+      expect(titleEl.nativeElement.textContent.trim())
+          .toEqual('Welcome to stock-market!');
+
+      // Check for default stock values in template
+      const nameEl = fixture.debugElement.query(By.css('.name'));
+      expect(nameEl.nativeElement.textContent)
+          .toEqual('Test Stock Company (TSC)');
+      const priceEl = fixture.debugElement.query(By.css('.price.positive'));
+      expect(priceEl.nativeElement.textContent).toEqual('$ 85');
+      const addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeDefined();
+    });
+ });
+});
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
