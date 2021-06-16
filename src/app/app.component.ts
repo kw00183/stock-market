9c0cf32e57c70d301fd4c5385beb194c099765b5
kw00183 <kw00183@cs6261-vm08.uwg.westga.edu>
2021-06-15 23:05:13 -0400

    add in hooks to the app and stock-item files to show order of component load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, SimpleChanges, OnInit, OnChanges, OnDestroy,
+         DoCheck, AfterViewChecked, AfterViewInit, AfterContentChecked,
+         AfterContentInit } from '@angular/core';
 import { Stock } from '../app/model/stock';
 
 @Component({
@@ -6,35 +8,43 @@ import { Stock } from '../app/model/stock';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges, OnDestroy,
+                                     DoCheck, AfterContentChecked,
+                                     AfterContentInit, AfterViewChecked,
+                                     AfterViewInit {
   title = 'app works!';
 
   public stock: Stock;
-  private counter: number = 1;
-
-  ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company - ' + this.counter++,
-        'TSC', 85, 80);
-  }
 
   onToggleFavorite(stock: Stock) {
-    // This will update the value in the stock item component
-    // Because it is triggered as a result of an event
-    // binding from the stock item component
+    console.log('Favorite for stock ', stock, ' was triggered');
     this.stock.favorite = !this.stock.favorite;
   }
 
-  changeStockObject() {
-    // This will update the value in the stock item component
-    // Because we are creating a new reference for the stock input
-    this.stock = new Stock('Test Stock Company - ' + this.counter++,
-        'TSC', 85, 80);
+  ngOnInit(): void {
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    console.log('App Component - On Init');
   }
 
-  changeStockPrice() {
-    // This will not update the value in the stock item component
-    // because it is changing the same reference and angular will
-    // not check for it in the OnPush change detection strategy.
-    this.stock.price += 10;
+  ngAfterViewInit(): void {
+    console.log('App Component - After View Init');
+  }
+  ngAfterViewChecked(): void {
+    console.log('App Component - After View Checked');
+  }
+  ngAfterContentInit(): void {
+    console.log('App Component - After Content Init');
+  }
+  ngAfterContentChecked(): void {
+    console.log('App Component - After Content Checked');
+  }
+  ngDoCheck(): void {
+    console.log('App Component - Do Check');
+  }
+  ngOnDestroy(): void {
+    console.log('App Component - On Destroy');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('App Component - On Changes - ', changes);
   }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-
+import { Component, SimpleChanges, OnInit,
+         OnChanges, OnDestroy, DoCheck, AfterViewChecked,
+         AfterViewInit, AfterContentChecked,
+         AfterContentInit, Input,
+         Output, EventEmitter } from '@angular/core';
 import { Stock } from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.Default
+  styleUrls: ['./stock-item.component.css']
 })
-export class StockItemComponent {
+export class StockItemComponent implements OnInit, OnChanges,
+                                           OnDestroy, DoCheck,
+                                           AfterContentChecked,
+                                           AfterContentInit,
+                                           AfterViewChecked,
+                                           AfterViewInit {
 
   @Input() public stock: Stock;
   @Output() private toggleFavorite: EventEmitter<Stock>;
@@ -22,7 +28,28 @@ export class StockItemComponent {
     this.toggleFavorite.emit(this.stock);
   }
 
-  changeStockPrice() {
-    this.stock.price += 5;
+  ngOnInit(): void {
+    console.log('Stock Item Component - On Init');
+  }
+  ngAfterViewInit(): void {
+    console.log('Stock Item Component - After View Init');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Stock Item Component - After View Checked');
+  }
+  ngAfterContentInit(): void {
+    console.log('Stock Item Component - After Content Init');
+  }
+  ngAfterContentChecked(): void {
+    console.log('Stock Item Component - After Content Checked');
+  }
+  ngDoCheck(): void {
+    console.log('Stock Item Component - Do Check');
+  }
+  ngOnDestroy(): void {
+    console.log('Stock Item Component - On Destroy');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Stock Item Component - On Changes - ', changes);
   }
 }
