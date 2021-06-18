2a1ea148e92660fc368a7db009ac27794929b11e
kw00183 <kw00183@cs6261-vm12.uwg.westga.edu>
2021-06-17 20:33:35 -0400

    update the create stock component constructor to use FormBuilder

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -8,12 +9,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateStockComponent {
 
-  public stockForm: FormGroup = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0)])
-  });
-  constructor() {}
+  public stockForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
 
   onSubmit() {
     console.log('Stock Form Value', this.stockForm.value);
